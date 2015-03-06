725e87751cd4f3b5550b484f6fdb2c9c4a33b39a
Dylan Rhodes <dylanr@stanford.edu>
2015-03-06 00:20:32 -0800

    Switch back to proper training, adjust model parameters

--- convert_to_leveldb.py
+++ convert_to_leveldb.py
@@ -63,7 +63,7 @@ def fill_in_db(img_list, database):
 	database.close()
 
 print 'PROCESSING TRAINING DATA...\n'
-fill_in_db(train_list[:512], data_db)
+fill_in_db(train_list, data_db)
 
 print 'PROCESSING TEST DATA...\n'
-fill_in_db(train_list[:512], test_db)
+fill_in_db(test_list, test_db)
